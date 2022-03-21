53592f18ac56402a8d3ec8b42e0bdce4b142bb3c
055055 <snb4301@gmail.com>
2022-03-21 22:23:16 +0900

    path 충돌로 인한 path 변경

--- url-shortener/src/main/java/com/musinsa/url/config/WebConfig.java
+++ url-shortener/src/main/java/com/musinsa/url/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("forward:/v1/short-url");
-        registry.addViewController("/v1/short-url").setViewName("short-url-form.html");
+        registry.addViewController("/").setViewName("forward:/v1/short-urls");
+        registry.addViewController("/v1/short-urls").setViewName("short-url-form.html");
         registry.addViewController("/v1/short-url/table").setViewName("short-url-table.html");
     }
 }
